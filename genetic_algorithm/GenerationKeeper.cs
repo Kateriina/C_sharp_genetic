1c6461830182cdfe6c04ed021665335d6200d816
Kateriina <47224878+Kateriina@users.noreply.github.com>
2019-11-26 17:57:24 +0300

    Create class Population

--- genetic_algorithm/GenerationKeeper.cs
+++ genetic_algorithm/GenerationKeeper.cs
@@ -13,15 +13,34 @@ namespace genetic_algorithm
         //vozvrashaet potomkov list
         public List<Chromosomes> Crossingover(Chromosomes x, Chromosomes y)
         {
+            //mutaciya hromosom x and y
+            x.Mutation();
+            y.Mutation();
+
+            //dlya randomnoj tochki razriva
+            int a = _rnd.Next(1, 7);
+            int b = size - a;
+
+
             //ya hochu vernut dve hromosomi - odna hrom x0y1, vtoraya y0x1
 
-            Chromosomes x0y1 = new Chromosomes(x.X.GetRange(0, 4).Concat(y.X.GetRange(4, 4)).ToList());
+            Chromosomes x0y1 = new Chromosomes(x.X.GetRange(0, a).Concat(y.X.GetRange(a, b)).ToList());
 
-            Chromosomes y0x1 = new Chromosomes(y.X.GetRange(0, 4).Concat(x.X.GetRange(4, 4)).ToList());
+            Chromosomes y0x1 = new Chromosomes(y.X.GetRange(0, a).Concat(x.X.GetRange(a, b)).ToList());
 
             return new List<Chromosomes>(){ x0y1, y0x1};
         }
-        
+
+        //mne nuzhno sdelat' populyaciyu
+        public List<int> Population { get; set; }
+        public void CreateNewPopulation()
+        {
+            Population = new List<int>();
+
+            Chromosomes chrom = new Chromosomes();
+            Population.Add(chrom);
+
+        }
 
     }
 }
